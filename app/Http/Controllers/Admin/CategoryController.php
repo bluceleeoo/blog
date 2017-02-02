683d7bd0bb4f045f3504158f08de25d8951e7f81
bluceleeoo <836376040qq.com>
2017-02-02 08:41:55 +0800

    32讲，后台文章分类编辑，1
    Signed-off-by: bluceleeoo <836376040qq.com>

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use App\Http\Model\Category;
 use Illuminate\Http\Request;
 use App\Http\Requests;
@@ -11,7 +9,7 @@ use Illuminate\Support\Facades\Validator;
 class CategoryController extends CommonController{
     //get.admin/category全部分类列表
     public function index(){
-        $categorys=(new Category)->tree();
+        $categorys = (new Category)->tree();
 //        dd($categorys);
 //        echo 'get.admin/category';
         //$data=$this->getTree($categorys,'cate_name','cate_id','cate_pid');
@@ -58,11 +56,10 @@ class CategoryController extends CommonController{
         $message=[
             'cate_name.required'=>'分类名称不能为空！',
         ];
-
         $validator=Validator::make($input,$rules,$message);
         if($validator->passes()){
             $re = Category::create($input);
-            //dd($re);
+            dd($re);
             if($re){
                 return redirect('admin/category');
             }else{
@@ -74,20 +71,21 @@ class CategoryController extends CommonController{
             return back()->withErrors($validator);
         }
     }
-    //get.admin/category/{category}显示单个分类信息
-    public function show(){
-
-    }
-    //delete.admin/category/{category}删除单个分类
-    public function destroy(){
+    //get.admin/category/{category}/edit编辑分类
+    public function edit(){
 
     }
     //put.admin/category/{category}更新分类
     public function update(){
 
     }
-    //get.admin/category/{category}/edit编辑分类
-    public function edit(){
+
+    //get.admin/category/{category}显示单个分类信息
+    public function show(){
+
+    }
+    //delete.admin/category/{category}删除单个分类
+    public function destroy(){
 
     }
 
