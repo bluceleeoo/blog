a67efd194ec7b500de679ff15181fb77e62f4dd3
bluceleeoo <836376040qq.com>
2017-02-03 16:01:05 +0800

    59讲，图文列表和分页读取成功，ok；
    Signed-off-by: bluceleeoo <836376040qq.com>

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Http\Model\Article;
+use App\Http\Model\Links;
 use App\Http\Model\Navs;
 
 class IndexController extends CommonController
 {
     public function index()
     {
-       //$navs = Navs::all();
-        return view('home.index');
+       //点击量最高的6片文章
+        $hot = Article::orderBy('art_view','desc')->take(6)->get();
+        //dd($hot);
+        //图文列表（带分页）
+        $data= Article::orderBy('art_time','desc')->take(5)->paginate(5);
+        //dd($data);
+        //最新发布8文章
+        $new= Article::orderBy('art_time','desc')->take(8)->get();
+        //友情链接
+        $links=Links::orderBy('link_order','asc')->get();
+        //dd($links);
+        //网站配置项
+        return view('home.index',compact('hot','data','new','links'));
     }
 
     public function cate()
